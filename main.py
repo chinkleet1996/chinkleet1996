711ad015366fb241eb11883aec648db514eea55d
chinkleet shukla <33054430+chinkleet1996@users.noreply.github.com>
2023-01-27 06:30:03 +0000

    Basic string operations

--- main.py
+++ main.py
@@ -1,60 +1,24 @@
-# below uses the indexing in which we can get the position of a specific character in our string
-print("Hello\n"[4])
+#write your code below this line 👇
+print("Hello"+ " " + "Chinks"+ " " + "How are you")
 
-# below code indicates that anthing inside the double qoutes will be treated as string in python
-print("123"+"456\n")
 
-# now below code used integer data type that sum up two integer numbers
-print(123 + 456)
+#for creating space between different strings we can both the above and below techniques
+print("Hello "+"Chinkleet"+" How are you\n")
 
-#Float data type
-3.154
 
-#Boolean
-True
-False
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.\n")
 
-num_char = len(input("What is your class name? "))
 
-# str method can be used to chnage the data type
-new_num_char = str(num_char)
-print("your name has " + new_num_char + " number of characters.\n")
+#input() will get the user input in the console
+# Then print will print the  Hello and user input
+print("Hello "+ input("What is your name\n"))
 
-# type method to use in case of no idea of the data type
-print(type(num_char))
+
+#below code will take the input from the user and calucate the lenght of the string
+print(len(input("What is your school name?\n")))
 
 
 
-# below are some examples for data type changing / type casting method
-
-a = float(1298)
-print(a)
-
-print(70 + float(1250))
-print("\n")
-
-
-# below code first takes the input from the user
-# then pick out the 0th and 1st index digits
-# then convert the string value to the equivalent int data type
-# finally perform the addition between the numbers.
-two_digit_number = input("Type a two digit number: ")
-print(int(two_digit_number[0]) + int(two_digit_number[1]))
-print("\n")
-
-# below is the example of different operator and their working preecedence
-# computation always from the left to right side
-# so first it will multiply (4 * 4) = 16
-# then comes to the div part (4 / 4) = 1.0
-# then comes to add part 16 + 1.0 = 17.0
-# finally sub comes into the picture 17.0 - 4 = 13.0
-print( 4 * 4 + 4 / 4 - 4)
-
-
-
-# below code shows the working of a BMI calculater which takes height and weight as an input and shows BMI value.
-
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-BMI = int(weight) / float(height) ** 2
-print(int(BMI))
